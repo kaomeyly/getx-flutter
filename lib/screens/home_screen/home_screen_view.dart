151a7b785365c5b76574c01df5ff737d64daa4a1
Kao Meyly <kaomeyly@gmail.com>
2026-04-21 15:54:57 +0700

    get stoage

--- lib/screens/home_screen/home_screen_view.dart
+++ lib/screens/home_screen/home_screen_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:getx_project/routes/app_routes.dart';
 
 import 'homescreen_controller.dart';
 
@@ -9,13 +11,7 @@ class HomeScreenView extends GetView<HomescreenController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Screen"),
-        leading: IconButton(
-          onPressed: () => Get.back(),
-          icon: Icon(Icons.arrow_back),
-        ),
-      ),
+      appBar: AppBar(title: Text("Home Screen")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -30,6 +26,16 @@ class HomeScreenView extends GetView<HomescreenController> {
               style: TextStyle(fontSize: 20),
             ),
             Text("Password  :  ", style: TextStyle(fontSize: 20)),
+
+            ElevatedButton(
+              onPressed: () {
+                var box = GetStorage();
+                // box.remove("key");
+                box.erase();
+                Get.toNamed(AppRoutes.login);
+              },
+              child: Text("Logout"),
+            ),
           ],
         ),
       ),
